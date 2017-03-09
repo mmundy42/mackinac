01bc6087440bb56349063f63fb148a7c326fc75a
Mike Mundy <mundy.michael@mayo.edu>
2017-03-09 16:35:54 -0600

    Switch to bumpversion for managing version number

--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-from os.path import abspath, dirname, join
-from sys import argv, path
 from setuptools import setup, find_packages
-
-# To temporarily modify sys.path
-SETUP_DIR = abspath(dirname(__file__))
-
-# Import version to get the version string
-path.insert(0, join(SETUP_DIR, 'mackinac'))
-from version import get_version, update_release_version
-path.pop(0)
-version = get_version(pep440=True)
-
-# If building something for distribution, ensure the VERSION
-# file is up to date
-if 'sdist' in argv or 'bdist_wheel' in argv:
-    update_release_version()
 
 requirements = [
     'cobra>=0.5.4',
@@ -31,14 +15,11 @@ except:
 
 setup(
     name='mackinac',
-    version=version,
+    version='0.8.0',
     packages=find_packages(),
     setup_requires=[],
     install_requires=requirements,
     tests_require=['pytest'],
-    package_data={
-        '': ['VERSION']
-    },
     author='Michael Mundy, Helena Mendes-Soares, Nicholas Chia',
     author_email='mundy.michael@mayo.edu',
     description='Mackinac: A bridge between ModelSEED and COBRApy',
@@ -46,7 +27,7 @@ setup(
     license='BSD',
     keywords='metabolism biology optimization flux balance analysis fba',
     url='https://github.com/mmundy42/mackinac',
-    # download_url='https://pypi.python.org/pypi/mackinac',
+    download_url='https://pypi.python.org/pypi/mackinac',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
