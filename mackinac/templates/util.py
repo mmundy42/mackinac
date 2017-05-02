2f97547c88fc41048ab67b2114a216036ad63a4a
Mike Mundy <mundy.michael@mayo.edu>
2017-05-02 16:55:48 -0500

    Add make_search_name() function

--- mackinac/templates/util.py
+++ mackinac/templates/util.py
@@ -7,6 +7,24 @@ from cobra.core import DictList
 # Regular expression for compartment suffix on a universal metabolite ID
 universal_compartment_suffix_re = re.compile(r'_([\d]+)$')
 
+# Regular expression to match EC number notation
+ec_number_re = re.compile(r'\(EC ([\d\-]+\.[\d\-]+\.[\d\-]+\.[\d\-]+)\)')
+
+# Regular expression to match TC number notation
+tc_number_re = re.compile(r'\(TC ([\d\-]+\.[\w]+\.[\d\-]+\.[\d\-]+\.[\d\-]+)\)')
+
+# Regular expression to match "NAD(P)" string (details below)
+nadp_re = re.compile(r'NAD\(P\)')
+
+# Regular expression to remove whitespace
+whitespace_re = re.compile(r'\s')
+
+# Regular expression to remove comments
+comment_re = re.compile(r'#.*$')
+
+# Regular expression to remove special characters
+special_chars_re = re.compile(r'[-;:()\[\]\',>]*')
+
 
 class DuplicateError(Exception):
     """ Exception raised when a duplicate ID is found in a source file. """
@@ -117,3 +135,38 @@ def change_compartment(metabolite, new_compartment):
     stripped_id = re.sub(universal_compartment_suffix_re, '', metabolite.id)
     metabolite.id = '{0}_{1}'.format(stripped_id, new_compartment)
     return metabolite
+
+
+def make_search_name(name):
+    """ Make a search name from a name.
+
+    A search name is created by removing special characters, whitespace,
+    comments, EC numbers, and TC numbers from a name.
+    
+    Parameters
+    ----------
+    name : str
+        Name to convert to a search name
+        
+    Returns
+    -------
+    str
+        Search name string
+    """
+
+    search_name = name
+    # Remove EC number
+    search_name = re.sub(ec_number_re, '', search_name)
+    # Remove TC number
+    search_name = re.sub(tc_number_re, '', search_name)
+    # Switch parenthesis to curly brace so other parenthesis can be removed
+    search_name = re.sub(nadp_re, 'NAD{P}', search_name)
+    # Lower case everything
+    search_name = search_name.lower()
+    # Remove whitespace
+    search_name = re.sub(whitespace_re, '', search_name)
+    # Remove comments from the end
+    search_name = re.sub(comment_re, '', search_name)
+    # Remove special characters
+    search_name = re.sub(special_chars_re, '', search_name)
+    return search_name
